331135df64356239874717cbd4357151b4bf503e
Jaden <blazerunner44@gmail.com>
2015-08-17 16:44:42 -0600

    Added sliding animation

--- slider.jquery.js
+++ slider.jquery.js
@@ -2,14 +2,15 @@ $.fn.slider = function(){
 	function transition(current,next){
 		// alert('current is: ' + current);
 		// alert('next is:' + next);
-		container.children('div.slide').eq(current).hide();
-		container.children('div.slide').eq(next).slideDown();
+		container.children('div.slide').eq(current).hide().css('left', '100%');
+		container.children('div.slide').eq(next).show().animate({left: '0'});
 	}
 	var container = this;
 	this.find('.slide').hide();
 	this.find('.slide:nth-of-type(1)').show();
 	this.append('<div class="forward arrow"> > </div>');
 	this.prepend('<div class="backward arrow"> < </div>');
+	this.children('.slide').eq(0).css('left', '0');
 
 	// calculate top margin of arrows for vertical center
 	$('.arrow').css('margin-top', this.height()/2-100);
@@ -36,5 +37,21 @@ $.fn.slider = function(){
 			transition(slide, slide-1);
 			slide -= 1;
 		}
-	})
+	});
+
+	//Bind left and right keys
+	$(document).keydown(function(e) {
+	    switch(e.which) {
+	        case 37: // left
+	        	$('.backward').trigger('click');
+	        break;
+
+	        case 39: // right
+	        	$('.forward').trigger('click');
+	        break;
+
+	        default: return; // exit this handler for other keys
+	    }
+	    e.preventDefault(); // prevent the default action (scroll / move caret)
+	});
 }
